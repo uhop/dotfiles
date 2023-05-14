5db2e1010ebe29d6603a6d8d1ffce374cef8d00f
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2023-05-14 14:28:45 -0500

    Split off:
    * the setup for Debian
    * the setup for vim

---
+++ run_once_after_install-vim.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Install the Ultimate VIM: https://github.com/amix/vimrc
+if [ ! -d ~/.vim_runtime ]; then
+  git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+  sh ~/.vim_runtime/install_awesome_vimrc.sh
+fi
