500730490299387fce4d457a94ff08a53fad1ad4
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-03-21 12:14:25 -0500

    Added the first cut of terminal colors.

---
+++ private_dot_local/bin/colors.sh
@@ -0,0 +1,60 @@
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+RESET_COLOR=$(tput setaf 9)
+
+FG_BLACK=BLACK
+FG_RED=RED
+FG_GREEN=GREEN
+FG_YELLOW=YELLOW
+FG_BLUE=BLUE
+FG_MAGENTA=MAGENTA
+FG_CYAN=CYAN
+FG_WHITE=WHITE
+FG_RESET=RESET_COLOR
+
+BG_BLACK=$(tput setab 0)
+BG_RED=$(tput setab 1)
+BG_GREEN=$(tput setab 2)
+BG_YELLOW=$(tput setab 3)
+BG_BLUE=$(tput setab 4)
+BG_MAGENTA=$(tput setab 5)
+BG_CYAN=$(tput setab 6)
+BG_WHITE=$(tput setab 7)
+BG_RESET=$(tput setab 9)
+
+BOLD=$(tput bold)
+DIM=$(tput dim)
+UNDERLINE_ON=$(tput smul)
+UNDERLINE_OFF=$(tput rmul)
+REVERSE=$(tput rev)
+BLINK=$(tput blink)
+INVISIBLE=$(tput invis)
+STANDOUT_ON=$(tput smso)
+STANDOUT_OFF=$(tput rmso)
+ITALIC_ON=$(tput sitm)
+ITALIC_OFF=$(tput ritm)
+RESET=$(tput sgr0)
+
+CURSOR_SAVE=$(tput sc)
+CURSOR_RESTORE=$(tput rc)
+CURSOR_HOME=$(tput home)
+CURSOR_DOWN1=$(tput cud1)
+CURSOR_UP1=$(tput cuu1)
+CURSOR_LEFT1=$(tput cub1)
+CURSOR_RIGHT1=$(tput cuf1)
+CURSOR_INVISIBLE=$(tput civis)
+CURSOR_NORMAL=$(tput cnorm)
+CURSOR_CLEAR_BEGIN=$(tput el1)
+CURSOR_CLEAR_END=$(tput el)
+CURSOR_LAST=$(tput ll)
+
+SCREEN_SAVE=$(tput smcup)
+SCREEN_RESTORE=$(tput rmcup)
+SCREEN_CLEAR=$(tput clear)
+SCREEN_CLEAR_END=$(tput ed)
