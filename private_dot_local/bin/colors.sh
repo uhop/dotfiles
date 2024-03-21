a72e178272d6ec4e8ebb4fb2a80d0d8c1b27da0e
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-03-21 13:50:42 -0500

    Added more tput commands.

--- private_dot_local/bin/colors.sh
+++ private_dot_local/bin/colors.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 BLACK=$(tput setaf 0)
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
@@ -28,6 +30,9 @@ BG_CYAN=$(tput setab 6)
 BG_WHITE=$(tput setab 7)
 BG_RESET=$(tput setab 9)
 
+color_fg () { return "$(tput setaf "$1")"; }
+color_bg () { return "$(tput setab "$1")"; }
+
 BOLD=$(tput bold)
 DIM=$(tput dim)
 UNDERLINE_ON=$(tput smul)
@@ -40,6 +45,19 @@ STANDOUT_OFF=$(tput rmso)
 ITALIC_ON=$(tput sitm)
 ITALIC_OFF=$(tput ritm)
 RESET=$(tput sgr0)
+
+TEXT_BOLD=BOLD
+TEXT_DIM=DIM
+TEXT_UNDERLINE_ON=UNDERLINE_ON
+TEXT_UNDERLINE_OFF=UNDERLINE_OFF
+TEXT_REVERSE=REVERSE
+TEXT_BLINK=BLINK
+TEXT_INVISIBLE=INVISIBLE
+TEXT_STANDOUT_ON=STANDOUT_ON
+TEXT_STANDOUT_OFF=STANDOUT_OFF
+TEXT_ITALIC_ON=ITALIC_ON
+TEXT_ITALIC_OFF=ITALIC_OFF
+TEXT_RESET=RESET
 
 CURSOR_SAVE=$(tput sc)
 CURSOR_RESTORE=$(tput rc)
@@ -54,7 +72,20 @@ CURSOR_CLEAR_BEGIN=$(tput el1)
 CURSOR_CLEAR_END=$(tput el)
 CURSOR_LAST=$(tput ll)
 
+cursor_pos    () { return "$(tput cup "$1" "$2")"; }
+cursor_left   () { return "$(tput cub "$1")"; }
+cursor_right  () { return "$(tput cuf "$1")"; }
+cursor_clear  () { return "$(tput ech "$1")"; }
+cursor_insert () { return "$(tput ich "$1")"; }
+cursor_insert_lines () { return "$(tput il "$1")"; }
+
 SCREEN_SAVE=$(tput smcup)
 SCREEN_RESTORE=$(tput rmcup)
 SCREEN_CLEAR=$(tput clear)
 SCREEN_CLEAR_END=$(tput ed)
+
+screen_lines  () { return "$(tput lines)"; }
+screen_cols   () { return "$(tput cols)"; }
+screen_colors () { return "$(tput colors)"; }
+
+terminal_name () { return "$(tput longname)"; }
