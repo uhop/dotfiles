3b079ca920fe734171877f82a13106b354aa7b8a
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-03-23 11:05:05 -0500

    Added some color to the installation scripts.

--- run_once_after_install-vim.sh
+++ run_once_after_install-vim.sh
@@ -2,6 +2,9 @@
 
 # Install the Ultimate VIM: https://github.com/amix/vimrc
 if [ ! -d ~/.vim_runtime ]; then
+  CYAN=$(tput setaf 14)
+  RESET=$(tput op)
+  echo "${CYAN}Installing the Ultimate VIM...${RESET}"
   git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
   sh ~/.vim_runtime/install_awesome_vimrc.sh
 fi
