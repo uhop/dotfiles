74486c8069b6c8b13aeede795a9acabf2806d869
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2025-04-13 20:11:31 -0500

    Updated `tput` values.

--- run_once_after_install-vim.sh
+++ run_once_after_install-vim.sh
@@ -2,8 +2,8 @@
 
 # Install the Ultimate VIM: https://github.com/amix/vimrc
 if [ ! -d ~/.vim_runtime ]; then
-  CYAN=$(tput setaf 14)
-  RESET=$(tput op)
+  CYAN=$(tput setaf 6)
+  RESET=$(tput sgr0)
   echo "${CYAN}Installing the Ultimate VIM...${RESET}"
   git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
   sh ~/.vim_runtime/install_awesome_vimrc.sh
