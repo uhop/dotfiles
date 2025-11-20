fec8c9d104d2c79b242e7987298095e7cdf42322
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2025-11-19 22:43:36 -0600

    Refactored scripts to keep all init in the main.

--- private_dot_local/libs/main.sh
+++ private_dot_local/libs/main.sh
@@ -1,3 +1,11 @@
+# include options.bash
+command -v git &> /dev/null && git -C ~/.local/share/libs/scripts pull > /dev/null || true
+
+. ~/.local/share/libs/scripts/ansi.sh
+. ~/.local/share/libs/scripts/args.sh
+. ~/.local/share/libs/scripts/args-version.sh
+. ~/.local/share/libs/scripts/args-help.sh
+
 # echo the first argument and run
 echoRun() {
   echo -e "\033[36m$@\033[0m"
@@ -8,3 +16,6 @@ echoRunBold() {
   echo -e "\033[1;36m$@\033[0m"
   eval "$@"
 }
+
+# set up doas
+command -v doas &>/dev/null && [ -f /etc/doas.conf ] && alias sudo='doas' || true
